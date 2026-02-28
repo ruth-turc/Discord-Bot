ca90060b3c73865bb28b290c6329fa331c8549f0
ruth-turc <ruth.turc@gmail.com>
2026-02-28 11:51:51 -0600

    Commands
    Added two slash commands: hello and echo

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
-class Client(discord.Client):
+class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
+        try:
+            guild = discord.Object(id=1467243643869069314)
+            synced = await self.tree.sync(guild=guild)
+            print(f'Synced {len(synced)} commands to guild {guild.id}')
+        except Exception as e:
+            print(f'Error syncing commands: {e}')
+
 
     # How bot responds when there is a message
     async def on_message(self, message):
@@ -12,6 +21,7 @@ class Client(discord.Client):
         user_message = str(message.content)
         channel = str(message.channel.name)
 
+    
         #stops the bot from responding to iteslf
         if message.author == client.user:
             return
@@ -23,7 +33,7 @@ class Client(discord.Client):
         
         # if user sends message containing "clanker," bot responds
         if "clanker" in user_message.lower():
-            await message.channel.reply(f'Watch your language, roger roger')
+            await message.channel.send(f'Watch your language, roger roger')
             return
         
     # greets new members   
@@ -37,6 +47,22 @@ class Client(discord.Client):
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix="!", intents=intents)
 
-client = Client(intents=intents)
-client.run('')
+GUILD_ID = discord.Object(id=1467243643869069314)
+
+@client.tree.command(name="hello", description="Say hello", guild=GUILD_ID)
+async def sayHello(interaction: discord.Interaction):
+    await interaction.response.send_message("Greetings, roger roger!")
+
+
+@client.tree.command(name="echo", description="Echoes what you type with a roger roger", guild=GUILD_ID)
+async def echo(interaction: discord.Interaction, printer: str):
+    if "clanker" in printer.lower():
+        await interaction.response.send_message("HOW DARE YOU MAKE ME SAY THAT FOWL WORD, roger roger")
+    else: 
+        await interaction.response.send_message(printer + " roger roger")
+
+
+
+client.run('MTQ2NzI0MDgwODIyNTgzMzI3Mg.GtDtdT.sMimh0032pOh0xLztsC_Gf7eRd28gvGeRp-94s')
